f6e9a082df2cb7fc24d94b3f077b260de30a6a19
Горелова Екатерина ЗП-41 <katya.gorelova.18@gmail.com>
2025-01-28 23:08:08 +0700

    Added equals and hashCode to Manager class

--- src/com/sibsutis/employees/Manager.java
+++ src/com/sibsutis/employees/Manager.java
@@ -1,32 +1,47 @@
 package com.sibsutis.employees;
 
- import com.sibsutis.devices.Device;
+import com.sibsutis.devices.Device;
 
 import java.util.List;
- public class Manager extends Employee{
-    public Manager(int id, String fullName, Department department) {
-       super(id, fullName, department);
-  }
+import java.util.Objects;
 
-   public Manager(int id, String fullName, Department department, String phone) {
-       super(id, fullName, department, phone);
-   }
+public class Manager extends Employee {
+   public Manager(int id, String fullName, Department department) {
+        super(id, fullName, department);
+    }
+
+    public Manager(int id, String fullName, Department department, String phone) {
+        super(id, fullName, department, phone);
+    }
 
    public Manager(int id, String fullName, Department department, List<Device> devices) {
-       super(id, fullName, department, devices);
-   }
+        super(id, fullName, department, devices);
+    }
 
-   public Manager(int id, String fullName, Department department, String phone, List<Device> devices) {
+    public Manager(int id, String fullName, Department department, String phone, List<Device> devices) {
         super(id, fullName, department, phone, devices);
-   }
+    }
 
+
+    @Override
+    public String getPosition() {
+        return "Manager";
+    }
 
    @Override
-  public String getPosition() {
-       return "Manager";
-  }
- @Override
-  public void print() {
+    public void print() {
         System.out.println("Manager: id=" + getId() + ", fullName=" + getFullName() + ", position=" + getPosition() + ", department=" + getDepartment() + ", phone=" + getPhone() + ", devices=" + getDevices());
+    }
+   @Override
+   public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Manager manager = (Manager) o;
+         return getId() == manager.getId() && Objects.equals(getFullName(), manager.getFullName()) && Objects.equals(getPhone(), manager.getPhone()) && Objects.equals(getDevices(), manager.getDevices()) && Objects.equals(getDepartment(), manager.getDepartment());
   }
- }
+
+    @Override
+   public int hashCode() {
+        return Objects.hash(getId(), getFullName(), getPhone(), getDevices(), getDepartment());
+    }
+}

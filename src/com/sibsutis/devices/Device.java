aafe01cf5e9dff4044132348a83d8c80353b149f
Горелова Екатерина ЗП-41 <katya.gorelova.18@gmail.com>
2025-01-28 23:13:39 +0700

    Added equals and hashCode to Device class

--- src/com/sibsutis/devices/Device.java
+++ src/com/sibsutis/devices/Device.java
@@ -1,25 +1,29 @@
 package com.sibsutis.devices;
 
-     public class Device {
-     private int id;
-    private String name;
+     import com.sibsutis.Printable;
 
-    public Device(int id, String name) {
-     this.id = id;
-    this.name = name;
-    }
-    
-   public int getId(){
-    return id;
-    }
-   public String getName(){
-   return name;
-   }
-    @Override
-   public String toString() {
-   return "Device{" +
-           "id=" + id +
-           ", name='" + name + '\'' +
-          '}';
-   }
-  }
+     import java.util.Objects;
+
+      public abstract class Device implements Printable {
+        private int id;
+       private String name;
+
+       public Device(int id, String name) {
+           this.id = id;
+          this.name = name='" + name + '\'' +
+               '}';
+        }
+
+         @Override
+       public boolean equals(Object o) {
+           if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Device device = (Device) o;
+            return id == device.id && Objects.equals(name, device.name);
+        }
+
+         @Override
+         public int hashCode() {
+            return Objects.hash(id, name);
+         }
+       }

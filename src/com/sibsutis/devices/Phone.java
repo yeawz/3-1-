6d25b51513e5c6f51add6931f9417341615eb122
Горелова Екатерина ЗП-41 <katya.gorelova.18@gmail.com>
2025-01-28 23:16:15 +0700

    Added equals and hashCode to Phone class

--- src/com/sibsutis/devices/Phone.java
+++ src/com/sibsutis/devices/Phone.java
@@ -1,17 +1,39 @@
-package com.sibsutis.devices;
+import java.util.Objects;
 
-public class Phone extends Device {
-    
-    public Phone(int id, int price) {
-        super(id, price, null);
-    }
+    public class Phone extends Device {
+        private String phoneNumber;
 
-    public Phone(int id, int price, String ip) {
-        super(id, price, ip);
-    }
+       public Phone(int id, String name, String phoneNumber) {
+            super(id, name);
+            this.phoneNumber = phoneNumber;
+        }
 
-    @Override
-    public String getDeviceType() {
-        return "Phone";
+         public String getPhoneNumber() {
+            return phoneNumber;
+       }
+
+         @Override
+        public void print() {
+             System.out.println("Phone: id=" + getId() + ", name=" + getName() + ", phone=" + phoneNumber);
+       }
+         @Override
+       public String toString() {
+             return "Phone{" +
+                    "id=" + getId() +
+                    ", name='" + getName() + '\'' +
+                     ", phoneNumber='" + phoneNumber + '\'' +
+                    '}';
+        }
+         @Override
+       public boolean equals(Object o) {
+             if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+             Phone phone = (Phone) o;
+            return getId() == phone.getId() && Objects.equals(getName(), phone.getName()) && Objects.equals(phoneNumber, phone.phoneNumber);
+       }
+
+       @Override
+       public int hashCode() {
+            return Objects.hash(getId(), getName(), phoneNumber);
+        }
     }
-}

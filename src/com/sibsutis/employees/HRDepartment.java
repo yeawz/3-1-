88910682852030c6d6857ec599981561225a3891
Горелова Екатерина ЗП-41 <katya.gorelova.18@gmail.com>
2025-01-28 23:01:57 +0700

    Added equals and hashCode to HRDepartment class

--- src/com/sibsutis/employees/HRDepartment.java
+++ src/com/sibsutis/employees/HRDepartment.java
@@ -1,16 +1,30 @@
 package com.sibsutis.employees;
 
-    public class HRDepartment extends Department{
-        public HRDepartment(int id) {
-            super(id);
-        }
+import java.util.Objects;
 
-        @Override
-        public String getDepartmentName() {
-            return "HR Department";
-       }
-       @Override
-        public void print() {
-            System.out.println("Department: id=" + getId() + ", name=" + getDepartmentName());
-        }
+public class HRDepartment extends Department {
+    public HRDepartment(int id) {
+        super(id);
     }
+
+    @Override
+    public String getDepartmentName() {
+        return "HR Department";
+    }
+     @Override
+    public void print() {
+        System.out.println("Department: id=" + getId() + ", name=" + getDepartmentName());
+    }
+    @Override
+   public boolean equals(Object o) {
+       if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+       HRDepartment that = (HRDepartment) o;
+       return getId() == that.getId();
+    }
+
+    @Override
+   public int hashCode() {
+         return Objects.hash(getId());
+   }
+}

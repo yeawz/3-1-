3b9e6b276cbeb6b00d1218b5aff9bb5e01699df8
Горелова Екатерина ЗП-41 <katya.gorelova.18@gmail.com>
2025-01-28 18:32:42 +0700

    Реализован класс Phone

---
+++ src/com/sibsutis/devices/Phone.java
@@ -0,0 +1,17 @@
+package com.sibsutis.devices;
+
+public class Phone extends Device {
+    
+    public Phone(int id, int price) {
+        super(id, price, null);
+    }
+
+    public Phone(int id, int price, String ip) {
+        super(id, price, ip);
+    }
+
+    @Override
+    public String getDeviceType() {
+        return "Phone";
+    }
+}

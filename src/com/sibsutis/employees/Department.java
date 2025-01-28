fd26fe0bbafe3371f380c47cea1b37e301dee05d
Горелова Екатерина ЗП-41 <katya.gorelova.18@gmail.com>
2025-01-28 22:58:41 +0700

    Added equals and hashCode to Department class

--- src/com/sibsutis/employees/Department.java
+++ src/com/sibsutis/employees/Department.java
@@ -1,26 +1,40 @@
-```java
-   package com.sibsutis.employees;
+package com.sibsutis.employees;
 
-   import com.sibsutis.Printable;
+import com.sibsutis.Printable;
 
-   public abstract class Department implements Printable {
-       private int id;
+import java.util.Objects;
 
-       public Department(int id) {
-           this.id = id;
-       }
+public abstract class Department implements Printable {
+    private int id;
 
-      public int getId() {
-         return id;
-        }
+    public Department(int id) {
+        this.id = id;
+    }
 
-       public abstract String getDepartmentName();
+    public int getId() {
+        return id;
+    }
 
+    public abstract String getDepartmentName();
 
-      @Override
-      public String toString() {
-           return "Department{" +
-               "id=" + id +
-              '}';
-        }
-   }
+   @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                '}';
+    }
+
+     @Override
+    public boolean equals(Object o) {
+       if (this == o) return true;
+       if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+         return Objects.hash(id);
+    }
+}
+

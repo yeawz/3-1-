03b6550c0ec87862464c0b0f99b1cdb918528abf
Горелова Екатерина ЗП-41 <katya.gorelova.18@gmail.com>
2025-01-28 23:07:02 +0700

    Added equals and hashCode to Employee class

--- src/com/sibsutis/employees/Employee.java
+++ src/com/sibsutis/employees/Employee.java
@@ -4,35 +4,34 @@ import com.sibsutis.Printable;
 import com.sibsutis.devices.Device;
 
 import java.util.List;
+import java.util.Objects;
 
 public abstract class Employee implements Printable {
     private int id;
-   private String fullName;
+    private String fullName;
     private String phone;
-   private List<Device> devices;
-   private Department department;
-
+    private List<Device> devices;
+    private Department department;
 
     public Employee(int id, String fullName, Department department) {
         this.id = id;
         this.fullName = fullName;
         this.department = department;
-   }
-
+    }
 
     public Employee(int id, String fullName, Department department, String phone) {
         this.id = id;
         this.fullName = fullName;
         this.department = department;
-       this.phone = phone;
-  }
+        this.phone = phone;
+    }
 
-   public Employee(int id, String fullName, Department department, List<Device> devices) {
+    public Employee(int id, String fullName, Department department, List<Device> devices) {
         this.id = id;
-       this.fullName = fullName;
+        this.fullName = fullName;
         this.department = department;
-       this.devices = devices;
-   }
+        this.devices = devices;
+    }
 
     public Employee(int id, String fullName, Department department, String phone, List<Device> devices) {
         this.id = id;
@@ -42,36 +41,49 @@ public abstract class Employee implements Printable {
         this.devices = devices;
     }
 
-
     public int getId() {
         return id;
     }
 
-   public String getFullName() {
+    public String getFullName() {
         return fullName;
     }
 
-   public String getPhone() {
+    public String getPhone() {
         return phone;
-   }
+    }
 
-  public List<Device> getDevices(){
-     return devices;
-     }
-    public Department getDepartment(){
-       return department;
-   }
+    public List<Device> getDevices() {
+        return devices;
+    }
 
-   public abstract String getPosition();
+    public Department getDepartment() {
+        return department;
+    }
 
-   @Override
-   public String toString() {
-        return "Employee{" +
+    public abstract String getPosition();
+
+     @Override
+    public String toString() {
+         return "Employee{" +
              "id=" + id +
-             ", fullName='" + fullName + '\'' +
+            ", fullName='" + fullName + '\'' +
             ", phone='" + phone + '\'' +
              ", devices=" + devices +
-              ", department=" + department +
-               '}';
+             ", department=" + department +
+            '}';
+    }
+
+   @Override
+    public boolean equals(Object o) {
+       if (this == o) return true;
+       if (o == null || getClass() != o.getClass()) return false;
+       Employee employee = (Employee) o;
+       return id == employee.id && Objects.equals(fullName, employee.fullName) && Objects.equals(phone, employee.phone) && Objects.equals(devices, employee.devices) && Objects.equals(department, employee.department);
+   }
+
+    @Override
+   public int hashCode() {
+       return Objects.hash(id, fullName, phone, devices, department);
     }
 }
